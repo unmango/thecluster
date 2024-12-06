04638edcf187cb00612bcca88a4eda945a8fe4d9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-05 21:31:32 -0600

    Refactored workspace loading and added tests
    Significant changes include:
    - Refactoring of the workspace loading process in loader.go, moving the logic to a separate function.
    - Addition of new test cases for the workspace loader in loader_test.go.
    - Creation of a new test suite in pulumi_suite_test.go.
    - Expansion of functionality in workspace.go, including error handling and additional checks.

--- pkg/workspace/pulumi/loader.go
+++ pkg/workspace/pulumi/loader.go
@@ -1,9 +1,6 @@
 package pulumi
 
 import (
-	"fmt"
-
-	"github.com/unmango/thecluster/internal"
 	"github.com/unmango/thecluster/pkg"
 )
 
@@ -12,19 +9,5 @@ type loader string
 var Loader loader = "Pulumi"
 
 func (loader) Load(ctx pkg.Context, path string) (pkg.Workspace, error) {
-	rel, err := ctx.Parse(path)
-	if err != nil {
-		return nil, err
-	}
-
-	if !IsWorkspace(ctx.Fs(), rel) {
-		return nil, fmt.Errorf("not a pulumi workspace: %s", path)
-	}
-
-	w, err := internal.LoadWorkspace(ctx.Fs(), rel)
-	if err != nil {
-		return nil, fmt.Errorf("loading workspace: %w", err)
-	}
-
-	return &Workspace{w}, nil
+	return Load(ctx, path)
 }
--- pkg/workspace/pulumi/workspace.go
+++ pkg/workspace/pulumi/workspace.go
@@ -1,20 +1,55 @@
 package pulumi
 
 import (
+	"fmt"
 	"path/filepath"
 
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	"github.com/spf13/afero"
 	"github.com/unmango/thecluster/internal"
+	"github.com/unmango/thecluster/pkg"
 )
 
 type Workspace struct {
 	internal.Workspace
 }
 
+func (w *Workspace) Install(ctx pkg.Context) error {
+	ws, err := auto.NewLocalWorkspace(ctx)
+	if err != nil {
+		return fmt.Errorf("loading workspace: %w", err)
+	}
+
+	return ws.Install(ctx, nil)
+}
+
 func IsWorkspace(fs afero.Fs, path string) bool {
 	stat, err := fs.Stat(
 		filepath.Join(path, "Pulumi.yaml"),
 	)
+	if err != nil {
+		stat, err = fs.Stat(
+			filepath.Join(path, "Pulumi.yml"),
+		)
+	}
 
 	return err == nil && !stat.IsDir()
+}
+
+func Load(ctx pkg.Context, path string) (pkg.Workspace, error) {
+	rel, err := ctx.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if !IsWorkspace(ctx.Fs(), rel) {
+		return nil, fmt.Errorf("not a pulumi workspace: %s", path)
+	}
+
+	w, err := internal.LoadWorkspace(ctx.Fs(), rel)
+	if err != nil {
+		return nil, fmt.Errorf("loading workspace: %w", err)
+	}
+
+	return &Workspace{w}, nil
 }
