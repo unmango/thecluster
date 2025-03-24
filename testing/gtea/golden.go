5dac277d86668faf25abc323c2f78d22e0041f29
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-23 19:50:25 -0500

    Remember why I ditched this earlier

--- testing/gtea/golden.go
+++ testing/gtea/golden.go
@@ -1,11 +1,14 @@
 package gtea
 
 import (
+	"fmt"
 	"io"
+	"reflect"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/x/exp/teatest"
 )
 
@@ -16,4 +19,42 @@ func RequireGolden(tm *teatest.TestModel) {
 	out, err := io.ReadAll(tm.FinalOutput(tb))
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	teatest.RequireEqualOutput(tb, out)
+}
+
+type golden struct{}
+
+func BeGolden() golden {
+	return golden{}
+}
+
+// Match implements types.GomegaMatcher.
+func (g golden) Match(actual interface{}) (success bool, err error) {
+	tm, ok := actual.(*teatest.TestModel)
+	if !ok {
+		return false, fmt.Errorf("expected a *teatest.TestModel, got %v", reflect.TypeOf(actual))
+	}
+
+	panic("unimplemented")
+}
+
+// FailureMessage implements types.GomegaMatcher.
+func (g golden) FailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
+}
+
+// NegatedFailureMessage implements types.GomegaMatcher.
+func (g golden) NegatedFailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
+}
+
+type finalModel tea.Model
+
+func HaveFinalModel(expected tea.Model) finalModel {
+	return expected
+}
+
+type finalOutput interface{}
+
+func HaveFinalOutput(expected interface{}) finalOutput {
+	return expected
 }
