21020f7570652b664982cec23826dd6cbe70a03a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-23 22:32:47 -0500

    Add a chdir option

--- cmd/thecluster.go
+++ cmd/thecluster.go
@@ -8,10 +8,39 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
+	"github.com/unmango/devctl/pkg/work"
 	"github.com/unmango/go/cli"
 	"github.com/unmango/thecluster/app"
 	"github.com/unmango/thecluster/project"
 )
+
+type Options struct {
+	work.ChdirOptions
+}
+
+func New() *cobra.Command {
+	opts := Options{}
+	cmd := &cobra.Command{
+		Use:   "thecluster",
+		Short: "Manage your CLUSTER with style",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := opts.Chdir(cmd.Context()); err != nil {
+				cli.Fail(err)
+			}
+
+			// https://github.com/charmbracelet/bubbletea/issues/860
+			if isatty.IsTerminal(os.Stdout.Fd()) {
+				runApp(cmd.Context())
+			} else {
+				run(cmd.Context())
+			}
+		},
+	}
+
+	_ = work.ChdirFlag(cmd, &opts.ChdirOptions, "")
+
+	return cmd
+}
 
 func runApp(ctx context.Context) {
 	if len(os.Getenv("DEBUG")) > 0 {
@@ -35,20 +64,5 @@ func run(ctx context.Context) {
 		cli.Fail(err)
 	} else {
 		fmt.Println("Project:", proj.Dir.Path())
-	}
-}
-
-func New() *cobra.Command {
-	return &cobra.Command{
-		Use:   "thecluster",
-		Short: "Manage your CLUSTER with style",
-		Run: func(cmd *cobra.Command, args []string) {
-			// https://github.com/charmbracelet/bubbletea/issues/860
-			if isatty.IsTerminal(os.Stdout.Fd()) {
-				runApp(cmd.Context())
-			} else {
-				run(cmd.Context())
-			}
-		},
 	}
 }
