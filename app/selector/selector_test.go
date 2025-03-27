15103cb3fa1554c3e315bc876524a20c311be357
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-27 11:28:55 -0500

    Move app into the selector

--- app/selector/selector_test.go
+++ app/selector/selector_test.go
@@ -1,83 +1,42 @@
 package selector_test
 
 import (
+	"context"
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/x/exp/teatest"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/unmango/devctl/pkg/work"
 	"github.com/unmango/thecluster/app/selector"
+	"github.com/unmango/thecluster/project"
 	"github.com/unmango/thecluster/testing/gtea"
 )
 
-type wrapper struct{ selector.Model }
-
-func (w wrapper) Init() tea.Cmd { return w.Model.Init() }
-func (w wrapper) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	w.Model, cmd = w.Model.Update(msg)
-	return w, cmd
-}
-func (w wrapper) View() string { return w.Model.View() }
-
-type item string
-
-func (item) Init() tea.Cmd                         { return nil }
-func (i item) Update(tea.Msg) (tea.Model, tea.Cmd) { return i, nil }
-func (i item) View() string                        { return string(i) }
-
 var _ = Describe("Selector", func() {
-	Describe("when no items are given", func() {
-		It("should render nothing", func() {
-			m := wrapper{selector.New()}
+	It("should render project path", func(ctx context.Context) {
+		m := selector.New()
+		m.Proj = &project.Project{Dir: work.Directory("/tests")}
 
-			tm := teatest.NewTestModel(GinkgoTB(), m)
-			tm.Send(tea.Quit())
+		tm := teatest.NewTestModel(GinkgoTB(), m)
+		tm.Send(tea.Quit())
 
-			gtea.RequireGolden(tm)
-			m = tm.FinalModel(GinkgoTB()).(wrapper)
-			Expect(m.View()).To(BeEmpty())
-		})
+		gtea.RequireGolden(tm)
+		m = tm.FinalModel(GinkgoTB()).(selector.Model)
+		Expect(m.View()).To(ContainSubstring("/tests"))
 	})
 
-	When("items are given", func() {
-		It("should render the items", func() {
-			m := wrapper{selector.New()}
+	It("should render errors", func(ctx context.Context) {
+		m := selector.New()
 
-			tm := teatest.NewTestModel(GinkgoTB(), m)
-			tm.Send(selector.Items([]tea.Model{item("test")}))
-			tm.Send(tea.Quit())
+		tm := teatest.NewTestModel(GinkgoTB(), m)
+		tm.Send(fmt.Errorf("Test error"))
+		tm.Send(tea.Quit())
 
-			gtea.RequireGolden(tm)
-			m = tm.FinalModel(GinkgoTB()).(wrapper)
-			Expect(m.View()).To(ContainSubstring("\u221f test"))
-		})
-
-		It("should render a cursor on the selected item", func() {
-			m := wrapper{selector.New()}
-
-			tm := teatest.NewTestModel(GinkgoTB(), m)
-			tm.Send(selector.Items([]tea.Model{item("test1"), item("test2")}))
-			tm.Send(tea.Quit())
-
-			gtea.RequireGolden(tm)
-			m = tm.FinalModel(GinkgoTB()).(wrapper)
-			Expect(m.View()).To(ContainSubstring("\u221f test1 <"))
-			Expect(m.View()).To(ContainSubstring("\u221f test2"))
-		})
-
-		It("should move the cursor when an arrow key is pressed", func() {
-			m := wrapper{selector.New()}
-
-			tm := teatest.NewTestModel(GinkgoTB(), m)
-			tm.Send(selector.Items([]tea.Model{item("test1"), item("test2")}))
-			tm.Send(tea.KeyMsg(tea.Key{Type: tea.KeyDown}))
-			tm.Send(tea.Quit())
-
-			gtea.RequireGolden(tm)
-			m = tm.FinalModel(GinkgoTB()).(wrapper)
-			Expect(m.View()).To(ContainSubstring("\u221f test1"))
-			Expect(m.View()).To(ContainSubstring("\u221f test2 <"))
-		})
+		gtea.RequireGolden(tm)
+		m = tm.FinalModel(GinkgoTB()).(selector.Model)
+		Expect(m.Proj).To(BeNil())
 	})
 })
