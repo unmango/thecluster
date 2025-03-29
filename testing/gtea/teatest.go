e28d3311571d119bc430cad884a1c2d904b54792
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-29 15:32:34 -0500

    Stub out other teatest matchers

--- testing/gtea/teatest.go
+++ testing/gtea/teatest.go
@@ -2,14 +2,48 @@ package gtea
 
 import tea "github.com/charmbracelet/bubbletea"
 
-type finalModel tea.Model
-
-func HaveFinalModel(expected tea.Model) finalModel {
-	return expected
+type finalModel struct {
+	m tea.Model
 }
 
-type finalOutput interface{}
+func HaveFinalModel(expected tea.Model) finalModel {
+	return finalModel{expected}
+}
 
-func HaveFinalOutput(expected interface{}) finalOutput {
-	return expected
+// Match implements types.GomegaMatcher.
+func (g *finalModel) Match(actual interface{}) (success bool, err error) {
+	panic("unimplemented")
+}
+
+// FailureMessage implements types.GomegaMatcher.
+func (g *finalModel) FailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
+}
+
+// NegatedFailureMessage implements types.GomegaMatcher.
+func (g *finalModel) NegatedFailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
+}
+
+type finalOutput struct {
+	out []byte
+}
+
+func HaveFinalOutput(expected []byte) finalOutput {
+	return finalOutput{expected}
+}
+
+// Match implements types.GomegaMatcher.
+func (g *finalOutput) Match(actual interface{}) (success bool, err error) {
+	panic("unimplemented")
+}
+
+// FailureMessage implements types.GomegaMatcher.
+func (g *finalOutput) FailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
+}
+
+// NegatedFailureMessage implements types.GomegaMatcher.
+func (g *finalOutput) NegatedFailureMessage(actual interface{}) (message string) {
+	panic("unimplemented")
 }
